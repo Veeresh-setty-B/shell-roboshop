666678d61fc9dc4b256686cb467d924045c2c60d
“Veeresh85” <veeresh.settyb@gmail.com>
2026-02-03 18:48:59 +0100

    Push

--- catalouge.sh
+++ catalouge.sh
@@ -7,17 +7,17 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+set -e
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
 
 if [ $USERID -ne 0 ]; then
 echo "Run the command with root user" | tee -a $LOGFILE
 else
 exit 1
 fi
-set -e
-trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
 
 VALIDATE(){
-if [ $1 -ne 0]; then
+if [ $1 -ne 0 ]; then
 echo "$2: Failure"
 else
 echo "$2: Success" | tee -a $LOGFILE
