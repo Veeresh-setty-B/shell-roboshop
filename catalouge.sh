3a811582a7add9d2d64940456a48b6e2862d4edc
“Veeresh85” <veeresh.settyb@gmail.com>
2026-02-03 18:42:44 +0100

    Push

--- catalouge.sh
+++ catalouge.sh
@@ -21,4 +21,12 @@ fi
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-VALIDATE() $? "Installed node"
+VALIDATE $? "Installed node"
+
+mkdir /app 
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+cd /app 
+unzip /tmp/catalogue.zip
