95d56664d41281054aef154e7ba5573024192980
“Veeresh85” <veeresh.settyb@gmail.com>
2026-02-03 18:50:17 +0100

    Push

--- catalouge.sh
+++ catalouge.sh
@@ -32,7 +32,3 @@ VALIDATE $? "Installed node"
 mkdir /app 
 
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
-cd /app 
-unzip /tmp/catalogue.zip
