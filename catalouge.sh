3d4ea95c19e4fadca1780fefa9324f5847137141
“Veeresh85” <veeresh.settyb@gmail.com>
2026-02-03 20:23:15 +0100

    Push

--- catalouge.sh
+++ catalouge.sh
@@ -7,6 +7,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.veereshsetty.online
 set -e
 trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
 
@@ -29,6 +31,14 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 VALIDATE $? "Installed nodejs"
 
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
+
 mkdir -p /app 
 
 
@@ -36,8 +46,32 @@ useradd --system --home /app --shell /sbin/nologin --comment "roboshop system us
 VALIDATE $? "Creating User"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
 cd /app 
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 unzip /tmp/catalogue.zip
 
 cd /app 
-npm install 
-VALIDATE $? "Installed npm"
+npm install &>>$LOGS_FILE
+VALIDATE $? "Installed npm" 
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable catalogue &>>$LOGS_FILE
+systemctl start catalogue
+VALIDATE $? "Starting and enabling catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"
